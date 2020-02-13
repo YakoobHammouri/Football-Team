e094db22fa9cc61329a170c1e5edbb39f58d024d
Yakoob Hammouri <jakoub-h2002@hotmail.com>
2020-02-13 14:56:22 +0200

    Update RequestService.js

--- src/Controllers/RequestService.js
+++ src/Controllers/RequestService.js
@@ -5,7 +5,7 @@ const apiCall = (remotUrl, callback) => {
     url: remotUrl,
     headers: {
       'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
-      'x-rapidapi-key': 'ebbdffef82msh4220b78cd4635c3p115d66jsn4859aa085a9f'
+      'x-rapidapi-key': `${process.env.Api_Key}`
     }
   };
   request(options, callback);
